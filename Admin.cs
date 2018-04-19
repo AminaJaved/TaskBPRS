83d0041922e0b4488a4878749252e2615e0e9225
AminaJaved <amina.javed420@gmail.com>
2018-04-19 09:27:43 +0500

    Admin and Admin DL

---
+++ Admin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+using System.Drawing;
+
+namespace TBPserver
+{
+    [DataContract]
+    public class Admin
+    {
+        private string sec_question;
+        private string sec_answer;
+        [DataMember]
+        public string Sec_question
+        {
+            get { return sec_question; }
+            set { sec_question = value; }
+        }
+
+        [DataMember]
+        public string Sec_answer
+        {
+            get { return sec_answer; }
+            set { sec_answer = value; }
+        }
+        private string adminName;
+        [DataMember]
+        public string AdminName
+        {
+            get { return adminName; }
+            set { adminName = value; }
+        }
+        private string adminPassword;
+        [DataMember]
+        public string AdminPassword
+        {
+            get { return adminPassword; }
+            set { adminPassword = value; }
+        }
+        private Image adminImage;
+        [DataMember]
+        public Image AdminImage
+        {
+            get { return adminImage; }
+            set { adminImage = value; }
+        }
+    }
+}
---
+++ AdminDL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace TBPserver
+{
+    public class AdminDL
+    {
+        public static ArrayList adminName = new ArrayList();
+    }
+}
